36fda98c4855793e108a6b3338d9339e1ff8f234
bengtssonaamanda <126580575+bengtssonaamanda@users.noreply.github.com>
2023-05-16 12:54:51 +0200

    Update home.php

--- home.php
+++ home.php
@@ -1,40 +1,78 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Health Dashboard - About us</title>
-    <link rel="stylesheet" type="text/css" href="style1.css">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
-    <div class="banner">
-        <ul class="nav navbar nav-pills">
-            <li class="nav-item active">
-              <a class="nav-link" href="landingpage.html">Startpage</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" aria-current="page" href="about_us.html">About us</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="register.php">Register</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="login.php">Login</a>
-            </li>
-          </ul>
-        <div class="content">
-          <h1>About us</h1>
-            <p>Introducing The Circle Fitness Health Tracker, a company dedicated to promoting healthy habits and providing individuals with the tools and resources they need to achieve their fitness goals.</p>
-            <p>At The Circle Fitness, we understand that living a healthy lifestyle can be challenging, which is why we've developed an innovative solution to help our users stay on track.</p>
-            <br><br>
-            <p>Our app allows users to log their workouts and nutrition, providing them with daily statistics to track their progress. With our app, users can easily see how many</p>
-            <p>calories they've consumed, track their exercise routine, and monitor their overall health and fitness.
-            <p>our app can help you achieve your goals. <br><br>
-            <p>Our app provides users with all they will need to reach their goals, making it easy to stay on track and achieve optimal results.</p>
-            <p>At The Circle Fitness, we believe that healthy living should be accessible to everyone. Our app is designed to be easy to use and affordable, making it a convenient solution for</p>
-            <p>anyone looking to improve their health and fitness. With The Circle Fitness Health Tracker, achieving your fitness goals has never been easier. Register today and start living your best life!</p>
-        </div>
-    </div>
-</body>
-</html>
+      <title>Health Dashboard</title>
+        <link rel="stylesheet" type="text/css" href="style1.css">
+              <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+              <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+              <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+          </head>
+          <body>
+              <div class="banner">
+                  <div class="sidebar" id="side">
+                      <img src="logovit.png" class="logo">
+                      <ul>
+                          <br><br>
+                          <br><br>
+                          <br><br>
+                        <li><a href="home.php">Home</a></li>
+                        <br>
+                        <li><a href="profile.php">Profile</a></li>
+                        <br>                       
+                        <li><a href="statistic.php">Statistic</a></li>
+                        <br>
+                        <li><a href="nutrition.php">Nutrition</a></li>
+                        <br>
+                        <li><a href="workout.php">Workout</a></li>
+                        <br>
+                        <li><a href="steps.php">Steps</a></li>
+                        <br>
+                        <li><a href="sleep.php">Sleep</a></li>
+                        <br>
+                        <li><a href="logout.php">Log out</a></li>
+                      </ul>
+                   </div>
+            <div class="content">
+                <h1>Home</h1><br><br>
+                <div class="button-grid">
+                    <button onclick="location.href='add_water.php'" title="Add a glass of water">
+                        <img src="water_glass.png" alt="Water glass" width="60px">
+                    </button>
+                    <button onclick="location.href='steps.php'" title="Add steps">
+                        <img src="shoe.png" alt="Shoe" width="60px">
+                    </button>
+                    <button onclick="location.href='workout.php'" title="Add workout">
+                        <img src="hantel.png" alt="Hantel" width="60px">
+                    </button>
+                    <button onclick="location.href='sleep.php'" title="Add sleep">
+                        <img src="sleep.png" alt="Sleep" width="60px">
+                    </button>
+                </div>
+                <br><br>
+                <h2>Manage users</h2>
+<?php
+include_once('template.php');
+require_once('sessions.php');
+
+$userid = $_SESSION['user_ID'];
+
+if (!isset($_SESSION["user_ID"]))
+    header("Location:landingpage.html");
+
+$query = <<<END
+    SELECT is_admin
+    FROM user
+    WHERE user_ID = '$userid'
+END;
+    $result = $mysqli->query($query);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_object();
+        $isadmin = $row->is_admin;
+    }
+if (isset($_SESSION["user_ID"]) AND $isadmin == 1) {
+    echo $admin;
+}
+require_once('footer.php');
+?>
+</div>
+</div>
