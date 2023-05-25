d38607c66219c29e38bc704f2ae209df1975587f
bengtssonaamanda <126580575+bengtssonaamanda@users.noreply.github.com>
2023-05-25 21:31:51 +0200

    Update sessions.php

--- sessions.php
+++ sessions.php
@@ -7,6 +7,7 @@ $session_ID = session_id();
 $page_open = date("Y-m-d H:i:s");
 $ip_address = $_SERVER['REMOTE_ADDR'];
 $page = $_SERVER['REQUEST_URI'];
+$browser = $_SERVER['HTTP_USER_AGENT'];
 
 if (isset($_SESSION["user_ID"])) {
     $user_ID = $_SESSION["user_ID"];
@@ -14,12 +15,11 @@ if (isset($_SESSION["user_ID"])) {
 
 
 $insert_query = <<<END
-    INSERT INTO sessions (user_ID, page_open, ip_address, page)
-    VALUES ('$user_ID', '$page_open', '$ip_address', '$page')
+    INSERT INTO sessions (user_ID, page_open, ip_address, page, browser)
+    VALUES ('$user_ID', '$page_open', '$ip_address', '$page', '$browser')
 END;
 
 $result = $mysqli->query($insert_query);
-
 ?>
 
 
